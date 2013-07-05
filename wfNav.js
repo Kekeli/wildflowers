6556147a72d25c3a84406f761b1040f27e14952f
Kelley Scott <kekeli.scott@gmail.com>
2013-07-05 13:40:53 -0400

    Initial commit

---
+++ wfNav.js
@@ -0,0 +1,71 @@
+//-----------------
+// writes navigation bar
+//-------------------
+
+
+function writeNavbar( topic ) {
+	
+var sepStr;
+sepStr = " | " ;
+
+var outStr;
+outStr = "onMouseOut=\"(window.status=''); return true\"" ;
+
+var linkStr;
+if ( topic == null) topic = "";
+
+document.writeln("<IMG SRC=images/daisy.gif>");
+
+
+	linkStr = "<a href=\"faqs.html\" ";
+	linkStr += "onMouseOver=\"(window.status='frequently asked questions'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">FAQs</a>" ;
+linkStr += sepStr ;
+document.writeln( linkStr );
+
+
+	linkStr = "<a href=\"staff.html\" ";
+	linkStr += "onMouseOver=\"(window.status='staff'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">Staff</a>" ;
+linkStr += sepStr ;
+document.writeln( linkStr );	
+
+	linkStr = "<a href=\"events.html\"  ";
+	linkStr += "onMouseOver=\"(window.status='news and schedule of events'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">News</a>" ;
+linkStr += sepStr ;
+document.writeln( linkStr );
+
+	linkStr = "<a href=\"photos.html\"  ";
+	linkStr += "onMouseOver=\"(window.status='farm photos'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">Photos</a>" ;
+linkStr += sepStr ;
+document.writeln( linkStr );
+
+
+	linkStr = "<a href=\"sales.html\"  ";
+	linkStr += "onMouseOver=\"(window.status='horses for sale'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">Sales</a>" ;
+linkStr += sepStr ;
+document.writeln( linkStr );
+
+	linkStr = "<a href=\"dirs.html\"  ";
+	linkStr += "onMouseOver=\"(window.status='map and directions'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">Directions</a>" ;
+linkStr += sepStr ;
+document.writeln( linkStr );
+
+
+	linkStr = "<a href=\"index.html\"  ";
+	linkStr += "onMouseOver=\"(window.status='home'); return true\"" ;
+	linkStr += outStr ;
+	linkStr += ">Home</a>" ;
+document.writeln( linkStr );
+
+}
